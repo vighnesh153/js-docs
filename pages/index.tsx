c3c8e8311ad976a3d4729a6b11390ef3259806ef
vighnesh153 <vighnesh.raut13@gmail.com>
2021-09-25 15:16:03 +0530

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Vighnesh's Docs | Notes</title>
+        <meta name="description" content="A simple notes and documentation storing site for my personal needs." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+
+      </main>
+    </div>
+  )
+}
