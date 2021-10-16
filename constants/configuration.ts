3097601dfbe83bd7366b10561ffff1d195747cb8
vighnesh153 <vighnesh.raut13@gmail.com>
2021-10-16 20:13:40 +0530

    refactor out the firebase config

--- constants/configuration.ts
+++ constants/configuration.ts
@@ -2,6 +2,15 @@
 const configuration = {
   ADMIN_EMAILS: ['me@vighnesh153.com', 'vighnesh.raut13@gmail.com', 'vighnesh.raut133@gmail.com'],
   FIREBASE: {
+    CONFIG: {
+      apiKey: 'AIzaSyAeYODLnJxZK9MYDCW9gL-dWXZUTysEvHE',
+      authDomain: 'rv-js-docs.firebaseapp.com',
+      projectId: 'rv-js-docs',
+      storageBucket: 'rv-js-docs.appspot.com',
+      messagingSenderId: '600915178178',
+      appId: '1:600915178178:web:58777388df0330b0c8815b',
+      measurementId: 'G-SHHZH0Z2ER',
+    },
     FIRESTORE: {
       COLLECTIONS: {
         FILE_META: 'file-meta',
--- services/firebase/firebase.ts
+++ services/firebase/firebase.ts
@@ -4,20 +4,12 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+import configuration from 'constants/configuration';
+
 import { googleProvider } from './firebaseAuth';
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAeYODLnJxZK9MYDCW9gL-dWXZUTysEvHE',
-  authDomain: 'rv-js-docs.firebaseapp.com',
-  projectId: 'rv-js-docs',
-  storageBucket: 'rv-js-docs.appspot.com',
-  messagingSenderId: '600915178178',
-  appId: '1:600915178178:web:58777388df0330b0c8815b',
-  measurementId: 'G-SHHZH0Z2ER',
-};
-
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(configuration.FIREBASE.CONFIG);
 
 const firebase = {
   auth: getAuth(firebaseApp),
