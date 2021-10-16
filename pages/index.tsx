a62fdd25e0978ed136711602067eb310c296b58c
vighnesh153 <vighnesh.raut13@gmail.com>
2021-10-16 20:12:37 +0530

    cleanup the homePage file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,9 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 
 import Resizable from 'components/Resizable';
 import Explorer from 'components/Explorer';
 import View from 'components/View';
-
-import ExplorerItem from 'models/ExplorerItem';
-
-import GlobalsContext from 'contexts/GlobalsContext';
-import useConfirmation from 'hooks/useConfirmation';
 
 const StyledMain = styled.main`
   display: flex;
@@ -22,22 +17,7 @@ const StyledMain = styled.main`
 `;
 
 const HomePage = () => {
-  const globalsContext = useContext(GlobalsContext);
-  const { confirmation } = useConfirmation();
-
-  const activeFileId = '123';
-  const [saveRequired, setSaveRequired] = useState<{ [fileId: string]: boolean }>({});
-
-  const onTabClose = (fileId: string) => {};
-
-  useEffect(() => {
-    globalsContext.setExplorerItems(items);
-    confirmation({
-      title: 'Please',
-      message: 'Are you sure you want to do this?',
-      onConfirm: () => null,
-    });
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <StyledMain>
@@ -52,54 +32,3 @@ const HomePage = () => {
 };
 
 export default HomePage;
-
-const items: ExplorerItem[] = [
-  {
-    id: 'dir-001',
-    name: 'Dir 1',
-    type: 'directory',
-    parentIds: [],
-  },
-  {
-    id: 'dir-002',
-    name: 'Dir 2',
-    type: 'directory',
-    parentIds: [],
-  },
-  {
-    id: 'dir-004',
-    name: 'Dir 4',
-    type: 'directory',
-    parentIds: ['dir-002'],
-  },
-  {
-    id: 'file-004',
-    name: 'File 4',
-    type: 'file',
-    parentIds: ['dir-002', 'dir-004'],
-  },
-  {
-    id: 'file-001',
-    name: 'File 1',
-    type: 'file',
-    parentIds: ['dir-001'],
-  },
-  {
-    id: 'file-002',
-    name: 'File 2',
-    type: 'file',
-    parentIds: ['dir-002'],
-  },
-  {
-    id: 'file-003',
-    name: 'File 3',
-    type: 'file',
-    parentIds: ['dir-003'],
-  },
-  {
-    id: 'dir-003',
-    name: 'Dir 3',
-    type: 'directory',
-    parentIds: [],
-  },
-];
