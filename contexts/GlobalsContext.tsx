ab0e069d5e0c5e5869a426dddba420bc1d8e35f3
vighnesh153 <vighnesh.raut13@gmail.com>
2021-10-17 10:22:51 +0530

    expand the parent tree on focussed file change

--- contexts/GlobalsContext.tsx
+++ contexts/GlobalsContext.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import ExplorerItem from 'models/ExplorerItem';
 
 interface GlobalsContextProps {
@@ -67,6 +67,36 @@ export const GlobalsContextProvider: React.FC = (props) => {
   const [expandedExplorerItemIds, setExpandedExplorerItemIds] = useState<string[]>([]);
   const [focussedFileId, setFocussedFileId] = useState<string | null>(null);
   const [focussedNodeId, setFocussedNodeId] = useState<string | null>(null);
+
+  const onFocussedFileChange = useCallback(
+    (focussedFileId: string | null) => {
+      // No file focussed yet
+      if (!focussedFileId) return;
+
+      // Find the focussed file
+      const explorerItem = explorerItems.find(
+        (explorerItem) => explorerItem.id === focussedFileId
+      ) as ExplorerItem;
+
+      // Convert the existing expanded nodes to set for ease of access
+      const expandedNodes = new Set(expandedExplorerItemIds);
+
+      // add the parent node ids of the current focussed file to expandedSet
+      explorerItem.parentIds.forEach((id) => expandedNodes.add(`directory___${id}`));
+
+      // Expand the public or private directory (as this is not included in the parentIds
+      expandedNodes.add(`directory___${explorerItem.isPrivate ? 'private' : 'public'}`);
+
+      // Update the expanded ids
+      setExpandedExplorerItemIds(Array.from(expandedNodes));
+    },
+    [explorerItems, expandedExplorerItemIds]
+  );
+
+  // Do something whenever the focussed file id changes.
+  useEffect(() => {
+    onFocussedFileChange(focussedFileId);
+  }, [focussedFileId]);
 
   const value = useMemo<GlobalsContextProps>(
     () => ({
